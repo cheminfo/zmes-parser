1f21dce327a97024fa13f04c87e3cea0d3c6401e
Luc Patiny <luc@patiny.com>
2026-03-26 09:14:27 +0100

    chore: update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    snapshotFormat: {
+      maxOutputLength: 1e8,
+    },
     coverage: {
       exclude: ['src/__tests__/**'],
       include: ['src/**/*.ts'],
